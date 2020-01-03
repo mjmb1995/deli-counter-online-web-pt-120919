07d31120453631493a3a939147ef1e1a69b261b8
Mark Bihag <markbihag330@gmail.com>
2020-01-03 02:49:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+
+def line 
+  if katz_deli.size === 0 
+    puts "The line is currently empty."
+  else
+    
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. you are number #{katz_deli.index(name) + 1} in line."
+end
